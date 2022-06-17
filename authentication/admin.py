6aaf10fbe673e11ab4e8e3d2fda8c772720b1c0a
Adarsh-sachan-kiwitech <adarsh.sachan@kiwitech.com>
2022-06-17 17:46:58 +0530

    upload

---
+++ authentication/admin.py
@@ -0,0 +1,14 @@
+
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'created_at']
+
+
+admin.site.register(User, UserAdmin)
---
+++ income/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from authentication.models import User
+# Create your models here.
+class Income(models.Model):
+    SOURCE_OPTION = [
+        ('SALARY', 'SALARY'),
+        ('BUSINESS', 'BUSINESS'),
+        ('SIDE-HUSTLES', 'SIDE-HUSTLES'),
+        ('OTHERS', 'OTHERS'),
+    ]
+    source = models.CharField(choices=SOURCE_OPTION, max_length=255)
+    amount = models.DecimalField(max_length=10, decimal_places=2, max_digits=255)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date = models.DateField(null=False,blank=False)
+
+    class Meta:
+        ordering: ['-date']
+
+    def __str__(self):
+        return str(self.owner)+'s income'
